f930c895447ab216c35f5c99670577dec6331a25
baramundi666 <mkrol@student.agh.edu.pl>
2024-01-21 00:04:42 +0100

    zmodyfikowalem wygenerowane dane, dodalem warunki integralnosci - wygenerowane zgadzaja sie z tymi warunkami

--- DEFINICJABAZYDANYCH.sql
+++ DEFINICJABAZYDANYCH.sql
@@ -13,16 +13,27 @@
 CREATE TABLE Courses (
     ServiceID int IDENTITY(3,4) PRIMARY KEY,
     CourseName varchar(50)  NOT NULL,
-    Type varchar(20)  check(Type in ('Online', 'Hybrid', 'Stationary')) NOT NULL,
-    StartDate datetime check(StartDate > '2018-01-01') NOT NULL,
+    Type varchar(20)  CHECK(Type in ('Online', 'Hybrid', 'Stationary')) NOT NULL,
+    StartDate datetime CHECK(StartDate >= '2019-01-01') NOT NULL,
     EndDate datetime   NOT NULL,
     PriceInAdvance money   NOT NULL,
     PriceWhole money  NOT NULL,
-	check (EndDate > StartDate),
-	check (PriceInAdvance < PriceWhole),
-    Limit int NULL
-
+    Limit int NULL,
+	CONSTRAINT CoursesDateCheck CHECK (
+	ISDATE(StartDate) = 1 AND
+	ISDATE(EndDate) = 1 AND
+	EndDate > StartDate
+	),
+	CONSTRAINT CoursesPriceCheck CHECK (
+	PriceInAdvance >= 0 AND
+	PriceWhole >= 0 AND 
+	PriceInAdvance <= PriceWhole
+	),
+	CONSTRAINT CoursesLimitCheck CHECK (
+	Limit > 0 OR Limit IS NULL
+	)
 );
+
 
 
 -- Table: Courses_attendance
@@ -30,7 +41,7 @@ CREATE TABLE Courses_attendance (
     ClassID int  NOT NULL,
     CustomerID int  NOT NULL,
     ModuleID int  NOT NULL,
-    Attendance varchar(10) check(Attendance in ('Present', 'Absent'))  NOT NULL,
+    Attendance varchar(10) CHECK(Attendance in ('Present', 'Absent'))  NOT NULL,
     CONSTRAINT Courses_attendance_pk PRIMARY KEY  (CustomerID,ClassID)
 );
 
@@ -40,12 +51,15 @@ CREATE TABLE Courses_hist (
     ModuleID int  NOT NULL,
     LecturerID int  NOT NULL,
     TranslatorID int  NOT NULL,
-	StartDate datetime check(StartDate > '2018-01-01') NULL,
+	StartDate datetime CHECK(StartDate >= '2019-01-01') NULL,
     EndDate datetime  NOT NULL,
-	Type varchar(20)  check(Type in ('Online', 'Hybrid', 'Stationary')) NOT NULL,
-	check (EndDate > StartDate),
+	Type varchar(20)  CHECK(Type in ('Online', 'Hybrid', 'Stationary')) NOT NULL,
 	LinkNagranie varchar(50) NULL,
-
+	CONSTRAINT Courses_histDateCheck CHECK (
+	ISDATE(StartDate) = 1 AND
+	ISDATE(EndDate) = 1 AND
+	EndDate > StartDate
+	)
 );
 
 
@@ -56,7 +70,7 @@ CREATE TABLE Customers (
     FirstName VARCHAR(50) CHECK (LEFT(FirstName, 1) = UPPER(LEFT(FirstName, 1))) NOT NULL,
     LastName VARCHAR(50) CHECK (LEFT(LastName, 1) = UPPER(LEFT(LastName, 1))) NOT NULL,
     Balance MONEY NOT NULL,
-    Email VARCHAR(50) NOT NULL,
+    Email VARCHAR(50) NOT NULL UNIQUE,
     City VARCHAR(50) CHECK (LEFT(City, 1) = UPPER(LEFT(City, 1))) NOT NULL,
     Address VARCHAR(50) NOT NULL,
     PostalCode VARCHAR(50) NOT NULL
@@ -68,9 +82,11 @@ CREATE TABLE Diplomas (
     DiplomaID int IDENTITY(1,1) PRIMARY KEY,
     ServiceID int  NOT NULL,
     CustomerID int  NOT NULL,
-    Date datetime  check(Date > '2022-01-01') NOT NULL,
+    Date datetime  CHECK(Date >= '2019-01-01') NOT NULL,
     Title varchar(30) CHECK (LEFT(Title, 1) = UPPER(LEFT(Title, 1))) NOT NULL,
-
+	CONSTRAINT DiplomasDateCheck CHECK (
+	ISDATE(Date) = 1
+	)
 );
 
 
@@ -78,7 +94,7 @@ CREATE TABLE Diplomas (
 CREATE TABLE Exams (
     ServiceID int  NOT NULL,
     CustomerID int  NOT NULL,
-    Grade float(1) check (Grade in (2.0,3.0,3.5,4.0,4.5,5.0))  NOT NULL,
+    Grade float(1) CHECK (Grade in (2.0,3.0,3.5,4.0,4.5,5.0))  NOT NULL,
     CONSTRAINT Exams_pk PRIMARY KEY  (ServiceID,CustomerID)
 );
 
@@ -88,16 +104,20 @@ CREATE TABLE Internships (
     ServiceID int  NOT NULL,
     InternshipName varchar(200) CHECK (LEFT(InternshipName, 1) = UPPER(LEFT(InternshipName, 1))) NOT NULL,
     InternshipDescription varchar(200)   NOT NULL,
-    StartDate datetime check(StartDate > '2018-01-01') NOT NULL,
+    StartDate datetime CHECK(StartDate >= '2019-01-01') NOT NULL,
     EndDate datetime  NOT NULL,
-	check (EndDate > StartDate),
+	CONSTRAINT InternshipsDateCheck CHECK (
+	ISDATE(StartDate) = 1 AND
+	ISDATE(EndDate) = 1 AND
+	EndDate > StartDate
+	)
 );
 
 -- Table: Internships_passed
 CREATE TABLE Internships_passed (
     InternshipID int  NOT NULL,
     CustomerID int  NOT NULL,
-    Passed varchar(3) check(Passed in ('Yes', 'No')) NOT NULL,
+    Passed varchar(3) CHECK(Passed in ('Yes', 'No')) NOT NULL,
     CONSTRAINT Internships_passed_pk PRIMARY KEY  (InternshipID,CustomerID)
 );
 
@@ -114,20 +134,27 @@ CREATE TABLE Lectures (
     LecturerID int  NOT NULL,
     TranslatorID int  NOT NULL,
     ServiceID int  NOT NULL,
-    Type varchar(20)  check(Type in ('Online', 'Hybrid', 'Stationary')) NOT NULL,
+    Type varchar(20)  CHECK(Type in ('Online', 'Hybrid', 'Stationary')) NOT NULL,
     Language varchar(50) CHECK (LEFT(Language, 1) = UPPER(LEFT(Language, 1))) NOT NULL,
 	LinkNagranie varchar(50) NULL,
-	StartDate datetime check(StartDate > '2018-01-01') NOT NULL,
+	StartDate datetime NOT NULL,
     EndDate datetime  NOT NULL,
 	Limit int NULL,
-	check(EndDate > StartDate),
+	CONSTRAINT LecturesDateCheck CHECK (
+	ISDATE(StartDate) = 1 AND
+	ISDATE(EndDate) = 1 AND
+	EndDate > StartDate
+	),
+	CONSTRAINT LecturesLimitCheck CHECK (
+	Limit > 0 OR Limit IS NULL
+	)
 );
 
 -- Table: Lectures_attendance
 CREATE TABLE Lectures_attendance (
     CustomerID int  NOT NULL,
     LectureID int  NOT NULL,
-    Attendance varchar(10) check(Attendance in ('Present', 'Absent'))  NOT NULL,
+    Attendance varchar(10) CHECK(Attendance in ('Present', 'Absent'))  NOT NULL,
     CONSTRAINT Lectures_attendance_pk PRIMARY KEY  (CustomerID,LectureID)
 );
 
@@ -145,7 +172,10 @@ CREATE TABLE Order_details (
     ServiceID int  NOT NULL,
     OrderID int  NOT NULL,
     UnitPrice money  NOT NULL,
-    CONSTRAINT Order_details_pk PRIMARY KEY  (ServiceID,OrderID)
+    CONSTRAINT Order_details_pk PRIMARY KEY  (ServiceID,OrderID),
+	CONSTRAINT Order_detailsUnitPriceCheck CHECK (
+	UnitPrice >= 0
+	)
 );
 
 
@@ -154,13 +184,23 @@ CREATE TABLE Order_details (
 CREATE TABLE Orders (
     OrderID int  IDENTITY(1,1) PRIMARY KEY,
     CustomerID int  NOT NULL,
-    OrderDate datetime check(OrderDate > '2018-01-01') NOT NULL,
+    OrderDate datetime CHECK(OrderDate >= '2019-01-01') NOT NULL,
     PaymentAssesed money  NOT NULL,
     PaymentPaid money  NOT NULL,
     PaymentWaived money  NOT NULL,
     DueDate datetime   NOT NULL,
-    OrderStatus varchar(10)  check(OrderStatus in ('Ordered', 'InCart'))  NOT NULL,
-	check(DueDate > OrderDate),
+    OrderStatus varchar(10) CHECK(OrderStatus in ('Ordered', 'InCart'))  NOT NULL,
+	CONSTRAINT OrdersDateCheck CHECK (
+	ISDATE(DueDate) = 1 AND
+	ISDATE(OrderDate) = 1 AND
+	DueDate > OrderDate
+	),
+	CONSTRAINT OrdersPaymentCheck CHECK (
+	PaymentAssesed > 0 AND
+	PaymentPaid > 0 AND
+	PaymentWaived >= 0 AND
+	PaymentAssesed >= PaymentPaid
+	)
 );
 
 -- Table: Services
@@ -168,7 +208,11 @@ CREATE TABLE Services (
     ServiceID int  PRIMARY KEY,
     PriceInAdvance money   NOT NULL,
     PriceWhole money  NOT NULL,
-	check (PriceInAdvance < PriceWhole),
+	CONSTRAINT ServicesPriceCheck CHECK (
+	PriceInAdvance >= 0 AND
+	PriceWhole >= 0 AND 
+	PriceInAdvance <= PriceWhole
+	)
 );
 
 -- Table: Single_Studies
@@ -176,11 +220,18 @@ CREATE TABLE Single_Studies (
     ServiceID int IDENTITY(4,4) PRIMARY KEY,
 	LectureID int NOT NULL,
     Major varchar(50)  CHECK (LEFT(Major, 1) = UPPER(LEFT(Major, 1))) NOT NULL,
-	Type varchar(20)  check(Type in ('Online', 'Hybrid', 'Stationary')) NOT NULL, 
+	Type varchar(20)  CHECK(Type in ('Online', 'Hybrid', 'Stationary')) NOT NULL, 
 	Limit int NULL,
     PriceInAdvance money   NOT NULL,
     PriceWhole money  NOT NULL,
-	check (PriceInAdvance < PriceWhole),
+	CONSTRAINT Single_StudiesPriceCheck CHECK (
+	PriceInAdvance >= 0 AND
+	PriceWhole >= 0 AND 
+	PriceInAdvance <= PriceWhole
+	),
+	CONSTRAINT Single_StudiesLimitCheck CHECK (
+	Limit > 0 OR Limit IS NULL
+	)
 );
 
 
@@ -190,14 +241,24 @@ CREATE TABLE Studies (
     ServiceID int IDENTITY(2,4) PRIMARY KEY,
     SyllabusID int  NOT NULL,
     Major varchar(50)  CHECK (LEFT(Major, 1) = UPPER(LEFT(Major, 1))) NOT NULL,
-    StartDate datetime check(StartDate > '2018-01-01') NOT NULL,
+    StartDate datetime CHECK(StartDate >= '2019-01-01') NOT NULL,
     EndDate datetime   NOT NULL,
     PriceInAdvance money  NOT NULL,
     PriceWhole money  NOT NULL,
-	check (EndDate > StartDate),
-	Check (PriceInAdvance < PriceWhole),
-Limit int NULL
-
+	Limit int NULL,
+	CONSTRAINT StudiesDateCheck CHECK (
+	ISDATE(StartDate) = 1 AND
+	ISDATE(EndDate) = 1 AND
+	EndDate > StartDate
+	),
+	CONSTRAINT StudiesPriceCheck CHECK (
+	PriceInAdvance >= 0 AND
+	PriceWhole >= 0 AND 
+	PriceInAdvance <= PriceWhole
+	),
+	CONSTRAINT StudiesLimitCheck CHECK (
+	Limit > 0 OR Limit IS NULL
+	)
 );
 
 
@@ -210,7 +271,9 @@ CREATE TABLE Subjects (
     SubjectDescription varchar(200) NOT NULL,
     Hours int  NOT NULL,
     Assessment varchar(30) CHECK(Assessment in ('Attendance', 'Exam', 'Internship'))NOT NULL,
-
+	CONSTRAINT SubjectsHoursCheck CHECK (
+	Hours > 0
+	)
 );
 
 
@@ -233,14 +296,12 @@ CREATE TABLE Translator (
     TranslatorID int  IDENTITY(1,1) PRIMARY KEY,
     FirstName varchar(50) CHECK (LEFT(FirstName, 1) = UPPER(LEFT(FirstName, 1))) NOT NULL,
     LastName varchar(50) CHECK (LEFT(LastName, 1) = UPPER(LEFT(LastName, 1))) NOT NULL,
-
 );
 
 -- Table: Languages
 CREATE TABLE Languages (
     LanguageID int  IDENTITY(1,1) PRIMARY KEY,
     LanguageName varchar(50)  NOT NULL,
-
 );
 
 -- Table: Translator_details
@@ -256,12 +317,20 @@ CREATE TABLE Translator_details (
 CREATE TABLE Webinars (
     ServiceID int  IDENTITY(1,4) PRIMARY KEY,
     WebinarName varchar(50)  CHECK (LEFT(WebinarName, 1) = UPPER(LEFT(WebinarName, 1))) NOT NULL,
-	StartDate datetime check(StartDate > '2018-01-01') NOT NULL,
+	StartDate datetime CHECK(StartDate >= '2019-01-01') NOT NULL,
     EndDate datetime  NOT NULL,
     PriceInAdvance money   NOT NULL,
     PriceWhole money  NOT NULL,
-	check( EndDate > StartDate),
-	check (PriceInAdvance < PriceWhole),
+	CONSTRAINT WebinarsDateCheck CHECK (
+	ISDATE(StartDate) = 1 AND
+	ISDATE(EndDate) = 1 AND
+	EndDate > StartDate
+	),
+	CONSTRAINT WebinarsPriceCheck CHECK (
+	PriceInAdvance >= 0 AND
+	PriceWhole >= 0 AND 
+	PriceInAdvance <= PriceWhole
+	)
 );
 --Table: Webinars_attendance
 CREATE TABLE Webinars_attendance (
@@ -277,11 +346,14 @@ CREATE TABLE Webinars_hist (
     ServiceID int  NOT NULL,
     LecturerID int  NOT NULL,
     TranslatorID int  NOT NULL,
-	StartDate datetime check(StartDate > '2018-01-01')  NOT NULL,
+	StartDate datetime CHECK(StartDate >= '2019-01-01')  NOT NULL,
     EndDate datetime  NOT NULL,	
 	LinkNagranie varchar(50)  NOT NULL,
-	check( EndDate > StartDate),
- 
+	CONSTRAINT Webinars_histDateCheck CHECK (
+	ISDATE(StartDate) = 1 AND
+	ISDATE(EndDate) = 1 AND
+	EndDate > StartDate
+	)
 );
 
 -- Reference: Webinars_hist_Lecturers (table: Webinars_hist)
