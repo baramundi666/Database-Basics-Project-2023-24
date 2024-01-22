bb2b87b105964ead92ee0c069ffc7f2ab6418fa3
WojtekWietrzny <wietrzny@student.agh.edu.pl>
2024-01-22 03:00:35 +0100

    funkcje cz.1

--- FUNCTIONSFILIP.sql
+++ FUNCTIONSFILIP.sql
@@ -163,3 +163,147 @@ BEGIN
         
     END
 END;
+
+
+-- FUNCTIONS-WOJTEK
+
+CREATE FUNCTION GetFutureEvents
+    ()
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT *
+    FROM Lectures
+    WHERE EndDate > GETDATE()
+
+    UNION
+
+    SELECT *
+    FROM Webinars_hist
+    WHERE EndDate > GETDATE()
+
+    UNION
+
+    SELECT *
+    FROM Courses_hist
+    WHERE EndDate > GETDATE()
+);
+
+CREATE FUNCTION GetCustomersWithBalanceBeneath
+    (@ThresholdMoney MONEY)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT *
+    FROM Customers
+    WHERE Balance < @ThresholdMoney
+);
+
+
+CREATE FUNCTION GetCustomerBalance
+	(@CustomerID INT)
+RETURNS MONEY
+AS
+BEGIN
+    DECLARE @CustomerBalance MONEY;
+
+    SELECT @CustomerBalance = Balance
+    FROM Customers
+    WHERE CustomerID = @CustomerID;
+
+    RETURN @CustomerBalance;
+END;
+
+CREATE FUNCTION GetClassIDsForModule
+	(@ModuleID INT)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT ClassID
+    FROM Courses_attendance
+    WHERE ModuleID = @ModuleID
+
+    UNION
+
+    SELECT ClassID
+    FROM Lectures_attendance
+    WHERE ModuleID = @ModuleID
+
+    UNION
+
+    SELECT ClassID
+    FROM Webinars_attendance
+    WHERE ModuleID = @ModuleID
+);
+
+CREATE FUNCTION GetLecturesForStudies
+	(@ServiceID INT)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT L.*
+    FROM Lectures AS L
+    INNER JOIN Studies AS S ON L.ServiceID = S.ServiceID
+    WHERE S.ServiceID = @ServiceID
+);
+
+CREATE FUNCTION GetSubjectsForSyllabus
+	(@SyllabusID INT)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT S.*
+    FROM Subjects AS S
+    INNER JOIN Syllabus_details AS SD ON S.SubjectID = SD.SubjectID
+    WHERE SD.SyllabusID = @SyllabusID
+);
+
+CREATE FUNCTION CheckInternshipPassed
+(
+    @CustomerID INT
+)
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @PassedStatus VARCHAR(3);
+
+    SELECT @PassedStatus = Passed
+    FROM Internships_passed
+    WHERE CustomerID = @CustomerID;
+
+    RETURN CASE WHEN @PassedStatus = 'Yes' THEN 1 ELSE 0; 
+END;
+
+CREATE FUNCTION GetDiplomasForCustomer
+	(@CustomerID INT)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT D.*
+    FROM Diplomas AS D
+    WHERE D.CustomerID = @CustomerID
+);
+
+
+CREATE FUNCTION CheckCustomerAttendance
+(
+    @CustomerID INT,
+    @LectureID INT
+)
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @Attendance BIT;
+
+    SELECT @Attendance = CASE WHEN COUNT(*) > 0 THEN 1 ELSE 0 END
+    FROM Lectures_attendance
+    WHERE CustomerID = @CustomerID AND LectureID = @LectureID;
+
+    RETURN @Attendance;
+END;
